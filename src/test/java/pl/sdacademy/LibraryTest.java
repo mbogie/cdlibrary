4006f7048a071b2a48dbd037badd035acb1b0719
dominisz <dominisz@gmail.com>
2017-11-20 20:57:50 +0100

    Skeleton for Library class tests

---
+++ src/test/java/pl/sdacademy/LibraryTest.java
@@ -0,0 +1,25 @@
+package pl.sdacademy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * http://dominisz.pl
+ * 20.11.2017
+ */
+class LibraryTest {
+
+    @Test
+    void searchByGenreOnEmptyCDShouldReturnEmptyList() {
+
+    }
+
+    @Test
+    void searchByGenreWithNonExistingGenreShouldReturnEmptyList() {
+
+    }
+
+    @Test
+    void searchByGenreShouldReturnSomeResults() {
+
+    }
+}
