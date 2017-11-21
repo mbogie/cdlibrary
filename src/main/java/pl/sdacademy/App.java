1d55581fb8ed56b720357c438dad263bace38e3c
dominisz <dominisz@gmail.com>
2017-11-21 20:27:31 +0100

    Add main menu

--- src/main/java/pl/sdacademy/App.java
+++ src/main/java/pl/sdacademy/App.java
@@ -1,17 +1,48 @@
 package pl.sdacademy;
 
-import java.time.LocalDate;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        CD cd = new CD("YES", "90125", "???", LocalDate.of(1980, 1, 1));
-        cd.addGenre(Genre.ROCK);
+public class App {
+
+    private Scanner scanner;
+
+    public App() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void processMainMenu() {
+        showMainMenu();
+        int input = readInput(1, 6);
+
+
+    }
+
+    private int readInput(int min, int max) {
+        boolean success;
+        int choice = 0;
+        System.out.print("Wpisz liczbe od " + min + " do " + max + ": ");
+        do {
+            success = true;
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (Exception e) {
+                success = false;
+            }
+        } while (!success || (choice < min || choice > max));
+        return choice;
+    }
+
+    private void showMainMenu() {
+        System.out.println("1. Wyswietl liste wszystkich plyt");
+        System.out.println("2. Wyswietl liste plyt po gatunku");
+        System.out.println("3. Wyswietl liste plyt po tytule");
+        System.out.println("4. Wyswietl liste plyt po tytule utworu");
+        System.out.println("5. Dodaj plyte");
+        System.out.println("6. Wyjdz z programu");
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.processMainMenu();
     }
 }
